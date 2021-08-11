e8ab5c9ac9cb652a013a6ecbce9d5569c2a6ccee
sunbenxin <sunbenxin@bitforex.com>
2021-08-11 17:26:50 +0800

    Add redis lock

--- src/go.mod
+++ src/go.mod
@@ -5,6 +5,7 @@ go 1.16
 require (
 	github.com/gin-gonic/gin v1.7.3
 	github.com/go-redis/redis/v8 v8.11.1
+	github.com/go-redsync/redsync/v4 v4.3.0
 	github.com/sirupsen/logrus v1.8.1
 	golang.org/x/sys v0.0.0-20210119212857-b64e53b001e4 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
--- src/redis/redis.go
+++ src/redis/redis.go
@@ -4,15 +4,51 @@ package redis
 import (
 	"com.github.sunbenxin/config"
 	"github.com/go-redis/redis/v8"
+	"github.com/go-redsync/redsync/v4"
+	"github.com/go-redsync/redsync/v4/redis/goredis/v8"
 	log "github.com/sirupsen/logrus"
 )
 
 var rdb *redis.Client
+
+var rlock *redsync.Redsync
 
 // Init ...
 func Init(cfg *config.RedisConfig) {
 	rdb = redis.NewClient(&redis.Options{
 		Addr: cfg.Host,
 	})
+
+	// Create a pool with go-redis (or redigo) which is the pool redisync will
+	// use while communicating with Redis. This can also be any pool that
+	// implements the `redis.Pool` interface.
+	// 多个pool支持quorum
+	pool := goredis.NewPool(rdb)
+
+	// Create an instance of redisync to be used to obtain a mutual exclusion
+	// lock.
+	rlock = redsync.New(pool)
 	log.Info("init redis success")
+
+}
+
+// test func for rlock
+func test() {
+	// Obtain a new mutex by using the same name for all instances wanting the
+	// same lock.
+	mutexname := "my-global-mutex"
+	mutex := rlock.NewMutex(mutexname)
+
+	// Obtain a lock for our given mutex. After this is successful, no one else
+	// can obtain the same lock (the same mutex name) until we unlock it.
+	if err := mutex.Lock(); err != nil {
+		panic(err)
+	}
+
+	// Do your work that requires the lock.
+
+	// Release the lock so other processes or threads can obtain a lock.
+	if ok, err := mutex.Unlock(); !ok || err != nil {
+		panic("unlock failed")
+	}
 }
