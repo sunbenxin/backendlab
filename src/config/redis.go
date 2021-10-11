d04339bbc2fb9d01fc32696a10dd0d6ae99dd393
ken.sun <ken.sun@tron.network>
2021-10-11 13:52:41 +0800

    Add redis host example

--- src/config/redis.go
+++ src/config/redis.go
@@ -3,5 +3,13 @@ package config
 
 // RedisConfig ...
 type RedisConfig struct {
+	// example: redis://host:port
+	/*
+		redis://HOST[:PORT][?db=DATABASE[&password=PASSWORD]]
+		redis://HOST[:PORT][?password=PASSWORD[&db=DATABASE]]
+		redis://[:PASSWORD@]HOST[:PORT][/DATABASE]
+		redis://[:PASSWORD@]HOST[:PORT][?db=DATABASE]
+		redis://HOST[:PORT]/DATABASE[?password=PASSWORD]
+	*/
 	Host string `yaml:"host"`
 }
